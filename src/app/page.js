70082fb6c2a5ab2197baf2cc2e91cb609bda4c10
Jesus <jesusvanegas3005@gmail.com>
2024-08-25 16:54:16 -0500

    primer commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import { HeroSection } from "@/components/component/hero-section"
+import { InicioSesion } from "@/components/component/inicio-sesion"
+
+function Home() {
+  return (
+    <div>
+      {/* <HeroSection /> */}
+      <InicioSesion />
+    </div>
+  )
+}
+
+export default Home
